8e458945978f4c587b98ead86d4abfac805a7e22
dengyq365 <yiqiden@gmail.com>
2019-07-29 21:49:40 +0800

    first commit

---
+++ homework.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+// homework 1.1
+// int main(){
+//     int a, b, c;
+//     scanf("%d%d%d",&a, &b, &c);
+//     printf("%d",b);
+//     return 0;
+// }
+
+// homework 1.2
+
+// int main(){
+//     char a;
+//     cin >> a;
+//     cout << " " << " " << a << endl;
+//     cout << " " << a << a << a << endl;
+//     cout << a << a << a << a << a << endl;
+//     cout << " " << a << a <<a << endl;
+//     cout << " " << " " << a << endl;
+//     return 0;
+// }
+
+
+// homework 1.3
+
+// int main(){
+//     char a;
+//     scanf("%c",&a);
+//     printf("%d",(int)a);
+//     return 0;
+// }
+
+// homework 1.4
+
+// int main(){
+//     int a;
+//     scanf("%d",&a);
+//     printf("%c",(char) a);
+//     return 0;
+// }
+
+//  homework 1.5
+// int main(){
+//     int a = 1;
+//     short b = 1;
+//     printf("%d %d",sizeof(a),sizeof(b));
+//     return 0;
+// }
+
+
+//  homework 1.6
+
+int main(){
+    float a;
+    double b;
+    printf("%d %d",sizeof(a),sizeof(b));
+    return 0;
+}
